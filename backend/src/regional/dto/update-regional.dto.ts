572ee19186b1c2bde44a6c085b72f73c79709924
Pedro Vieira <pedronicolas576@gmail.com>
2025-07-05 15:50:39 -0300

    Incluindo update de regional para atualização de cadastro

---
+++ backend/src/regional/dto/update-regional.dto.ts
@@ -0,0 +1,6 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateRegionalDto } from './create-regional.dto';
+
+export class UpdateRegionalDto extends PartialType(CreateRegionalDto) {
+  nome?: string;
+}
